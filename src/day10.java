bcb03bd2c267f33a517d59a663a17d9934521a05
Suryaprabha <prabhasurya514@gmail.com>
2026-06-16 16:04:15 +0530

    day10

--- src/day10.java
+++ src/day10.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
+import java.util.LinkedList;
+import java.util.Queue;
 
+/*import  java.util.ArrayList;
 public class day10 {
-}
+    static void main(){
+        String[] student ={"surya","suthaa","poorani","ram","supriya"};
+        System.out.println(student.length);
+        ArrayList<String> list=new ArrayList();
+        list.add("surya");
+        list.add("suthaa");
+        list.add("poorani");
+        0
+        list.add("ram");
+        list.add("supriya");
+        for(String s: list){
+            System.out.println(s);
+        }
+        list.remove("ram");
+        System.out.println();
+        System.out.println(list.getFirst());
+        System.out.println(list.getLast());
+        System.out.println();
+        for(String s:list){
+            System.out.println(s);
+        }
+    }
+}*/
+/*public class day10 {
+    static void main(){
+        LinkedList<String> student= new LinkedList<String>();
+        student.add("a");
+        student.add("b");
+        student.add("c");
+        student.add("d");
+        while(!student.isEmpty()){
+        process(student);
+        }
+    }
+        static void process(LinkedList<String>student){
+            String removed=student.removeFirst();
+            System.out.println("removed:"+removed);
+        }
+    }*/
