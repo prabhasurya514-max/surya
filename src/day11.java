2f5c010e593a14f7f76250ee05ec13d9d0e4cbe2
Suryaprabha <prabhasurya514@gmail.com>
2026-06-18 09:48:03 +0530

    string

--- src/day11.java
+++ src/day11.java
@@ -1,8 +1,9 @@
-import java.util.Set;
-import java.util.HashSet;
+import java.util.*;
+
 public class day11 {
+    /*
     public static void main(String[] args) {
-        Set<String> Set = new HashSet<>();
+                Set<String> Set = new HashSet<>();
         Set.add("student1");
         Set.add("student2");
         Set.add("student1");
@@ -12,5 +13,20 @@ public class day11 {
         for (String s : Set)
             System.out.println(s);
     }
+}*/
+    static void main() {
 
+        Map<String, Integer> person = new HashMap<>();
+        person.put("Ram", 89);
+        person.put("john", 78);
+        person.put("Aravindh", 67);
+        person.put("ramesh", 90);
+        System.out.println(person.get("percent"));
+        person.entrySet().forEach(entry -> {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        });
+    }
 }
+
+
+
