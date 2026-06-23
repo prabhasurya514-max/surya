a1a692d1a738c0d9dc50530c488624fb1cda636d
Suryaprabha <prabhasurya514@gmail.com>
2026-06-23 11:23:03 +0530

    throw

--- src/day15_java.java
+++ src/day15_java.java
@@ -1,11 +1,11 @@
-public class day15_java {
+/*public class day15_java {
     static void main() throws InterruptedException {
         Runnable chefTask =() -> {
             String [] items ={ "burger","noodles","coffee"};
             for (int i=0; i < items.length;i++){
                 System.out.println("cooking Item:"+ items [i]);
             try{
-                Thread.sleep(5000);
+                Thread.sleep(1000 );
             }catch (InterruptedException e){
                 throw new RuntimeException(e);
             }
@@ -14,5 +14,37 @@ public class day15_java {
         Thread chefThread= new Thread(chefTask,"chef-Thread");
         chefThread.start();
         chefThread.join();
+    }
+}*/
+public class day15_java{
+    static String[] items = {"Burger","Dosa","Coffee"};
+
+    private static int nextOrderIndex = 0;
+
+    private static final Object LOCK = new Object();
+
+    static void pickOrder(){
+        String order;
+        while(true){
+            synchronized(LOCK){
+                if(nextOrderIndex>=items.length){
+                    return;
+                }
+                order = items[nextOrderIndex];
+                nextOrderIndex++;
+            }
+            System.out.println(Thread.currentThread().getName()+" is preparing : "+order);
+        }
+    }
+
+    static void main() throws InterruptedException {
+        Thread chefOne = new Thread(()->pickOrder(), "chef-one");
+        Thread chefTwo = new Thread(()->pickOrder(), "chef-two");
+
+        chefOne.start();
+        chefTwo.start();
+
+        chefOne.join();
+        chefTwo.join();
     }
 }
